6f3713ac0bf6aa530205d6679eb0d629289c1990
Rachel Li <li.rachel.cheng@gmail.com>
2021-05-24 01:22:03 -0400

    Fixed rink prices, al percentage an dimplemented TakeFirst for all the fields

--- drinks/items.py
+++ drinks/items.py
@@ -3,25 +3,49 @@ from scrapy.item import Item, Field
 from datetime import datetime 
 from scrapy.loader.processors import MapCompose, TakeFirst
 
-def remove_chars(text):
+def remove_newline_tab(text):
     text = text.strip()
     return text
-   
+
+def remove_quotes(text):
+    # strip the unicode quotes
+    text = text.strip(u'\u201c'u'\u201d')
+    return text
+
+def remove_chars(text):
+    text = text.replace(u'\xa0', u' ')
+    return text
 
 
 class DrinksItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    drink_name = Field()
-    drink_price = Field(
-        input_processor=MapCompose(remove_chars)
+    drink_name = Field(
+        output_processor=TakeFirst()
     )
-    drink_link = Field()
-    drink_type = Field()
-    summary = Field()
-    drink_volume = Field()
-    alcohol_percentage = Field()
+    drink_price = Field(
+        input_processor=MapCompose(remove_newline_tab),
+        output_processor=TakeFirst()
+    )
+    drink_link = Field(
+        output_processor=TakeFirst()
+    )
+    drink_type = Field(
+        output_processor=TakeFirst()
+    )
+    summary = Field(
+        # input_processor=MapCompose(remove_quotes)
+        output_processor=TakeFirst()
+    )
+    drink_volume = Field(
+        input_processor=MapCompose(remove_chars),
+        output_processor=TakeFirst()
+    )
+    alcohol_percentage = Field(
+        output_processor=TakeFirst()
+    )
     origin_place = Field(
-        input_processor=MapCompose(remove_chars)
+        input_processor=MapCompose(remove_newline_tab),
+        output_processor=TakeFirst()
     )
 
--- drinks/spiders/spirits-spider.py
+++ drinks/spiders/spirits-spider.py
@@ -12,10 +12,8 @@ class SojuSpider(scrapy.Spider):
         'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/grappa',
         'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/liqueur-liquor-15015',
         'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/rum-15016',
-        'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/shochu-soju-15017',
-        'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/tequila-15018',
-        'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/vodka-15019',
-        'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/whisky-whiskey-15020']
+        'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/shochu-soju-15017'
+
 
     def parse(self, response):
         # drink_number_total = response.css('.spaced-character::text').get()
@@ -32,7 +30,7 @@ class SojuSpider(scrapy.Spider):
             # drink_number += 1
             loader = ItemLoader(item = DrinksItem(), selector = drink)
             loader.add_css('drink_name', '.product_name a::text')
-            loader.add_css('drink_price', '#content .price::text')
+            loader.add_css('drink_price',  '.price::text')
             loader.add_css('drink_link', '.product_name a::attr(href)')
             drink_item = loader.load_item()
 
@@ -50,7 +48,7 @@ class SojuSpider(scrapy.Spider):
         loader = ItemLoader(item=drink_item, response=response)
         loader.add_css('summary', '.product-text-content p::text')
         loader.add_css('drink_volume', 'dd b::text')
-        loader.add_css('alcohol_percentage', 'dt:nth-child(2) b::text')
+        loader.add_css('alcohol_percentage', 'dd:nth-child(3) span::text')
         loader.add_css('origin_place', 'dd:nth-child(5) span::text')
         loader.add_css('drink_type', '#widget_breadcrumb li~ li+ li a::text')
         yield loader.load_item()
