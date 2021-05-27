6177c1ca81202d1fe28a81ab687069a6eee813d5
Rachel Li <li.rachel.cheng@gmail.com>
2021-05-27 16:48:20 -0400

    added databases to a SQLITE db

--- drinks/pipelines.py
+++ drinks/pipelines.py
@@ -1,7 +1,39 @@
 from sqlalchemy.orm import sessionmaker
 from scrapy.exceptions import DropItem
+from drinks.models import Drink, db_connect, create_table
+import logging
 
 
-class DrinksPipeline:
+class SaveDrinksPipeline:
+    def __init__(self):
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    
     def process_item(self, item, spider):
+        #Save quotes in the database
+        session = self.Session()
+        drink = Drink()
+        drink.name = item['drink_name']
+        drink.price = item['drink_price']
+        drink.link = item['drink_link']
+        drink.type = item['drink_type']
+
+        drink.origin_place = item['origin_place']
+        drink.percentage= item['alcohol_percentage']
+        drink.volume = item['drink_volume']
+        drink.summary = item['summary']
+
+
+        try:
+            session.add(drink)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+
+        finally: 
+            session.close()
+
         return item
--- drinks/settings.py
+++ drinks/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'drinks.pipelines.DrinksPipeline': 300,
+   'drinks.pipelines.SaveDrinksPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
